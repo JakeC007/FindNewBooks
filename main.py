fbb7b52440e1f021c8b95af264fd0d9c72892f28
Remote-Jake <gchanen1@swarthmore.edu>
2020-04-26 16:00:12 -0400

    API keys imported

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python3
 """
 April 26, 2020
 Author Jake Chanenson
-
-
 """
+
+from secrets import key, secret, num
+
+def main():
+    print("Hello:")
+    print(key)
+    print(secret)
+    print(num)
+
+if __name__ == "__main__":
+    main()
