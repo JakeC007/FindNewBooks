22876f05f3c7443773044eb8757261955f452ac5
Remote-Jake <gchanen1@swarthmore.edu>
2020-04-26 20:23:26 -0400

    read in books via author

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Find New Books
 
 ## Setup
+1. If you are on ubuntu ensure that you are using `pip3` which can be installed with the command `sudo apt-get install python3-pip`
+2. Install the Goodreads package `pip3 install Goodreads`
+3. Ensure that all needed dependencies are installed. See the [Goodreads project github page for more information](https://github.com/sefakilic/goodreads)
+    -*Warning* The goodraeds project is no longer being maintained.
+4.
 
 ## TDD
 - goodreads API
 - set up email
-- 
+-
--- main.py
+++ main.py
@@ -4,13 +4,32 @@ April 26, 2020
 Author Jake Chanenson
 """
 
-from secrets import key, secret, num
+from secrets import key, secret
+from goodreads import client
+import time
 
 def main():
-    print("Hello:")
-    print(key)
-    print(secret)
-    print(num)
+    gc = client.GoodreadsClient(key, secret)
+    authorLst = ["chambers", "doctorow", "scalzi", "James S.A.", "Butcher", ]
+    bookLst = []
+    for author in authorLst:
+        a = gc.find_author(author)
+        print(a.name)
+        print(a.books)
+        bookLst.append(a.books)
+        print("----"*10)
+        time.sleep(1)
+    print(bookLst)
+
+    # author = gc.find_author("chambers")
+    # print(author.name)
+
+    # time.sleep(1)
+    # usr = gc.user(50914049)
+    # print(usr.name)
+    #author = gc.author(4763)
+    #print(author.books)
+
 
 if __name__ == "__main__":
     main()
