2ed2146572b3a1dce7c35cd2de47c7953a397730
Jake <36495719+JakeC007@users.noreply.github.com>
2020-04-28 02:06:19 -0400

    created

---
+++ secrets.py
@@ -0,0 +1,14 @@
+"""
+April 27, 2020
+Secrets file for the Find New Books Project (https://github.com/JakeC007/FindNewBooks)
+To make this project work you need to fill in the following fields below. Here are some tips:
+  -key and secret: you need your own Goodreads API keys. 
+  -usrID: This can be found if you log into Goodreads and click on 'MyBooks' the url will be goodreads.com/review/list/usrID
+  -emailPW: you need your own email account. Make sure this is the password for the email address sitting in the sender_email variable.
+Place Your Secrets Here
+"""
+key = " "        #Goodreads API key
+secret = " "     #Goodreads API secret 
+usrID = 1        #The userID of the goodreads user of whom you want to find new books for (in most cases this is you)
+emailPW = " "    #for botmail.py
+jakeEmail = ""   #set the destination email globally 
