0f62794fbbd8adbbb57bf0da85e62849dd0e546b
Jake <36495719+JakeC007@users.noreply.github.com>
2020-04-28 15:19:40 -0400

    created template for users to store their private data

--- secrets2.py
+++ secrets2.py
@@ -1,10 +1,11 @@
 """
 April 27, 2020
 Secrets file for the Find New Books Project (https://github.com/JakeC007/FindNewBooks)
-To make this project work you need to fill in the following fields below. Here are some tips:
+To make this project work you need to fill in the following fields below and rename this file. Here are some tips:
   -key and secret: you need your own Goodreads API keys. 
   -usrID: This can be found if you log into Goodreads and click on 'MyBooks' the url will be goodreads.com/review/list/usrID
   -emailPW: you need your own email account. Make sure this is the password for the email address sitting in the sender_email variable.
+
 Place Your Secrets Here
 """
 key = " "        #Goodreads API key
