ff66364b955731c2432fd92a082bf2c5a5cd2053
Remote-Jake <gchanen1@swarthmore.edu>
2020-04-27 22:38:04 -0400

    fancy str formatting v1

--- main.py
+++ main.py
@@ -19,25 +19,53 @@ def main():
     authorLst = miscFunc.list_user_authors(key, usrID, 'authors-follow')
     print("Pulled Authors")
     print("Grabbing First Author's Books")
+
     #get each author's book list, and keep the books that were published within 2 years
     for author in authorLst:
-        authorStr+= author+" "
-         tempAuthorBooks = miscFunc.find_books(key, author)
-         print("Pulled Next Author's Books")
-         for book in tempAuthorBooks:
-             if((book.publication_year() >=currentYear-2) and (book.publication_year() <= currentYear)):
-                 tempBookLst.append(book)
+        authorStr+= author+", "
+        tempAuthorBooks = miscFunc.find_books(key, author)
+        print("Pulled Next Author's Books")
+        for book in tempAuthorBooks:
+            if((book.publication_year() >=currentYear-2) and (book.publication_year() <= currentYear)):
+                tempBookLst.append(book)
 
     titlesUsrRead = miscFunc.list_user_book_t(key, usrID, 'read')
     for tb in tempBookLst:
-        if(tb.title() in titlesUsrRead):
+        if(tb.title() in titlesUsrRead): #TODO make this 'not in'
             bookLst.append(tb)
+
+    body = f"Hey, \n Some of your favorite authors have new books out that you haven't read: \n {unpack(bookLst)}"\
+           f"These books come from the following authors {authors(lst)} {', '.join(str(x) for x in authorLst)} If you wish to change which authors to follow,"
+    print(body)
+
+
+
+def unpack(books):
+    retStr = ' '
+    for book in books:
+        tempStr = '\t*{0} by {1}\n'.format(book.title(), book.author_name())
+        retStr+=tempStr
+    return retStr
+
+def authors(lst):
+    retStr = ' '
+    for i in range(len(lst)):
+        if i != len(lst)-1:
+            tempStr = '{0},'.format(lst[i])
+        else:
+            tempStr = 'and {0}'.format(lst[i])
+        retStr+=tempStr
+    return retStr
+
+
+
+
 
     #remove untitled books from list
 
     #write email
         #author string
-        #formatted lst 
+        #formatted lst
 
 
 
